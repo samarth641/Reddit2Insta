ab069e104b167c2238ad7fb201be2f93153a514d
samarth641 <samarthverulkar8@gmail.com>
2023-04-06 19:57:02 +0530

    First Commit to reddit2insta

---
+++ README.md
@@ -0,0 +1,89 @@
+# Instagram Reddit Meme Bot
+
+This bot scrapes memes from random subreddits and posts them to Instagram. It uses PRAW to scrape memes from Reddit and InstagramAPI to post memes to Instagram. 
+
+## Prerequisites
+
+- Python 3.8 or higher
+- pip
+- virtualenv
+- Instagram account
+- Reddit account
+
+## Installation
+
+1. Clone the repository and navigate into the directory:
+```sh
+git clone https://github.com/samarth641/Reddit2Insta.git
+```
+
+
+2. Install the required modules by Running this code In terminal:
+
+ ```sh 
+ pip install -r requirements.txt 
+ ```
+
+3. Create a Reddit application and a corresponding client ID and client secret. Instructions can be found [Here](https://github.com/reddit-archive/reddit/wiki/OAuth2).
+
+
+4. Create an Instagram account and generate an API key. You can skip this step and only use your instagram account username and password as we are only using that to connect to `InstagramAPI`  [Documentation Here](https://developers.facebook.com/docs/instagram-basic-display-api/getting-started).
+
+3. Go to `.env` file in the root of the directory and add the following variables with your own credentials:
+
+
+```python
+REDDIT_CLIENT_ID=<your_reddit_client_id>
+REDDIT_CLIENT_SECRET=<your_reddit_client_secret>
+REDDIT_USERNAME=<your_reddit_username>
+REDDIT_PASSWORD=<your_reddit_password>
+REDDIT_USER_AGENT=<your_reddit_user_agent>
+INSTA_USERNAME=<your_instagram_username>
+INSTA_PASSWORD=<your_instagram_password>
+```
+
+# Changing subreddits
+
+To change the subreddits that the bot scrapes memes from, edit the subreddits variable in the reddit2insta.py file. Add or remove subreddits as needed, and separate them by commas. For example:
+
+```python
+subreddits = ['subreddits1', 'subreddits2', 'subreddits3', 'subreddits4']
+```
+
+6.  Run the bot: 
+
+```python 
+python reddit2insta.py
+```
+
+
+## Configuration
+
+- `subreddits`: A list of subreddits to scrape memes from. Replace the default subreddits with your own.
+- `directory`: The directory where the downloaded memes will be saved.
+- `posted_memes.json`: A JSON file that keeps track of the memes that have been posted to Instagram.
+
+## Contributing
+
+Contributions are welcome! Please open an issue or pull request for any changes you would like to make.
+
+
+## 🔗 Social Links
+[![Buy me a Coffee ](https://img.shields.io/badge/buymeacoffee-FFBF00?style=for-the-badge&logo=buymeacoffee&logoColor=white)](hhttps://www.buymeacoffee.com/SamarthVerulkar)
+
+[![instagram](https://img.shields.io/badge/Instagram-bc2a8d?style=for-the-badge&logo=instagram&logoColor=white)](https://www.instagram.com/samarth_verulkar/)
+
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/samarth-verulkar-89255a227/)
+
+[![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/samarthverulkar)
+
+
+## Authors
+
+- [@Samarth Verulkar ](https://www.github.com/samarth641)
+
+## License
+
+This project is licensed under the MIT License. See the `LICENSE` file for more information.
+
+
---
+++ reddit2insta.py
@@ -0,0 +1,121 @@
+from InstagramAPI import InstagramAPI
+import praw
+import urllib
+import os
+import json
+import random
+import uuid
+from dotenv import load_dotenv
+from PIL import Image
+
+# Load credentials from .env file
+load_dotenv()
+# Reddit credentials
+reddit_client_id = os.getenv("REDDIT_CLIENT_ID")
+reddit_client_secret = os.getenv("REDDIT_CLIENT_SECRET")
+reddit_username = os.getenv("REDDIT_USERNAME")
+reddit_password = os.getenv("REDDIT_PASSWORD")
+reddit_user_agent = os.getenv("REDDIT_USER_AGENT")
+
+# Instagram credentials 2FA Should not be on or else  InstagramAPI will not be able to work 
+instagram_username = os.getenv("INSTA_USERNAME")
+instagram_password = os.getenv("INSTA_PASSWORD")
+
+# List of subreddits to scrape memes from Replace With subreddits you want to fetch the memes from you can add as many as you want 
+subreddits = ['subreddits1', 'subreddits2', 'subreddits3', 'subreddits4']
+
+# Creating Reddit instance here
+reddit = praw.Reddit(client_id=reddit_client_id, client_secret=reddit_client_secret, username=reddit_username, password=reddit_password, user_agent=reddit_user_agent)
+
+directory = "./memes"
+files = os.listdir(directory)
+
+for file_name in files:
+    file_path = os.path.join(directory, file_name)
+    if os.path.isfile(file_path):
+        # do something with the file
+        print(file_name) 
+
+# Creating Instagram instance here
+instagram = InstagramAPI(instagram_username, instagram_password)
+login = instagram.login()
+if not login:
+    print("Failed to log in to Instagram")
+    exit()
+
+# Load previously posted memes from JSON file
+if os.path.isfile("posted_memes.json"):
+    with open("posted_memes.json", "r") as f:
+        posted_memes = json.load(f)
+else:
+    posted_memes = []
+
+# Loop through subreddits
+while True:
+    subreddit = random.choice(subreddits)
+    # Get random post from subreddit
+    post = random.choice(list(reddit.subreddit(subreddit).new(limit=100)))
+    # Check if post is an image
+    if post.url.endswith(".jpg") or post.url.endswith(".jpeg") or post.url.endswith(".png"):
+        # Check if post is not a video
+        if not post.is_video:
+            # Check if post has already been posted
+            if post.id not in posted_memes:
+                # Generate unique filename
+                filename = os.path.join('memes', str(uuid.uuid4()) + os.path.splitext(post.url)[-1])
+                # Download image
+                urllib.request.urlretrieve(post.url, filename)
+                image_path = os.path.join('.', filename)
+                print(f"Downloaded {os.path.join('.', filename)}")
+                # Add post ID to posted_memes list
+                posted_memes.append(post.id)
+
+                # Check if image_path is not empty
+                if not image_path:
+                    continue
+
+                # Check if file has a valid image extension
+                valid_extensions = {'.jpg', '.jpeg', '.png', '.gif'}
+                if not any(file_name.endswith(ext) for ext in valid_extensions for file_name in os.listdir("memes")):
+                    print(f"Unknown file extension, skipping: {post.title}")
+                    continue
+
+                # Check image size and resize if necessary
+                with Image.open(image_path) as img:
+                    width, height = img.size
+                    if width != height:
+                        new_size = (min(width, height), min(width, height))
+                        new_image = Image.new("RGB", new_size, "white")
+                        if width > height:
+                            new_image.paste(img, (0, (width-height)//2))
+                        else:
+                            new_image.paste(img, ((height-width)//2, 0))
+                        new_image.save(image_path)
+                        print("Resized image:", post.title)
+
+                # Post image to Instagram
+                try:
+                    instagram.uploadPhoto(image_path, caption=post.title)
+                except Exception as e:
+                    print("Error posting to Instagram:", str(e))
+                    continue
+
+                # Save posted memes to JSON file
+                with open("posted_memes.json", "w") as f:
+                    json.dump(posted_memes, f)
+
+                print("Posted to Instagram:", post.title)
+
+                # Exit loop after posting one meme
+                break
+            else:
+                print("Post already posted:", post.title)
+        else:
+            print("Post is a video:", post.title)
+    else:
+        print("Unknown file extension, skipping:", post.title)
+
+    # Add the current subreddit back to the list of subreddits
+    subreddits.append(subreddit)
+
+
